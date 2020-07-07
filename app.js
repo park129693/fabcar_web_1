d6ceabfe9abbdc7dc41fac28dd5984c84876d6e8
park young jun <park129693@gmail.com>
2020-07-07 12:31:40 +0900

    farcar web 작성및 테이블화 테스트

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+var fs = require('fs')
+var bodyParser = require('body-parser')
+var cors = require('cors')
+//dotenv 실행
+require('dotenv').config()
+
+var apiRouter = require('./routes/Router')
+
+//view template setting
+app.set('views', path.resolve(__dirname+'/views'))
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+//cors 설정
+app.use(cors())
+//라우팅 파일 등록
+app.use('/',apiRouter)
+
+var port = process.env.PORT || 3000
+app.listen(port, ()=>{
+    console.log(`Server is Starting at http://localhost:${port}`)
+})
+
---
+++ routes/Router.js
@@ -0,0 +1,12 @@
+var router = require('express').Router()
+var queryUtil = require('../utils/Util')
+
+router.get('/', async(req, res, next)=>{
+    var result = await queryUtil()
+    var resultData =  await JSON.parse(result)
+    console.log(resultData)
+    // res.send(resultData)
+    res.render('index',{data:resultData})
+})
+
+module.exports = router;
